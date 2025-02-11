1357435c256fd53aea6d832ed85457d40db910c5
elenafuengar <elena.delafuenteg@gmail.com>
2025-02-11 18:46:09 +0100

    test: use iddefix new `compute_neffint` and `compute_ineffint` functions

--- tests/001_test_fft.py
+++ tests/001_test_fft.py
@@ -51,70 +51,46 @@ import neffint
 time = np.linspace(1e-11, 50e-9, 1000)
 f_fd = np.linspace(0, 5e9, 1000)
 Z_fd = DE_model.get_impedance(frequency_data=f_fd, wakelength=None) 
-adaptative = False
+adaptative = True #gives same results, just slower
 
-if not adaptative:
-    W = neffint.fourier_integral_fixed_sampling(
-        times=time,
-        frequencies=f_fd,
-        func_values=Z_fd,
-        pos_inf_correction_term=True,
-        neg_inf_correction_term=False,
-        interpolation="linear" # `pchip gives artificial imag. baseline`
-    )
-
-# Using adaptative freq. refining
-if adaptative:
-    def interpolation_error_norm(func_output, interpolant_output):
-        return np.abs(func_output - interpolant_output)
-
-    frequencies, func_arr = neffint.improve_frequency_range(
-        initial_frequencies=f_fd,
-        func=interp1d(f_fd, Z_fd, kind='linear', fill_value="extrapolate"),
-        interpolation_error_norm=interpolation_error_norm,
-        absolute_integral_tolerance=1e0, # The absolute tolerance the algorithm tries to get the error below
-        step_towards_inf_factor=2, # The multiplicative step size used to scan for higher and lower frequencies to add
-        bisection_mode_condition=None, # None (the default) here gives only logarithmic bisection when adding internal points
-        max_iterations=10000,
-    )
-
-    W = neffint.fourier_integral_fixed_sampling(
-        times=time,
-        frequencies=frequencies,
-        func_values=func_arr,
-        pos_inf_correction_term=True,
-        neg_inf_correction_term=False,
-        interpolation="linear"
-    )
-
+t, W = iddefix.compute_ineffint(f_fd, Z_fd, #for transverse
+                                 times=time,
+                                 plane='transverse',
+                                 adaptative=adaptative)
 
 # Compare wake functions
 
 W_de = DE_model.get_wake(time)
 fig, ax0 = plt.subplots(1, 1, figsize=(12,5))
 
-ax0.plot(time, W.imag/np.pi, #for transverse is imag!
-         lw = 2, c='tab:blue', linestyle='-', label='neffint wake function', alpha=0.8)
-
 ax0.plot(time, W_de,
-         lw = 2, c='tab:red', linestyle='-', label='DE fitting wake function', alpha=0.8)
+         lw = 2, c='tab:red', linestyle='-', alpha=0.8, label='DE fitting wake function')
+ax0.plot(time, W, #for transverse is imag!
+         lw = 1.5, c='tab:blue', linestyle='--', label='neffint wake function')
 
 ax0.set_xlabel('f [Hz]')
 ax0.set_ylabel('$[ABS](Z_{transverse})$ [$\Omega$]')
 ax0.legend(loc='best', fontsize=14)
 ax0.grid()
 plt.show()
-fig.savefig('001_compare_wakes.png')
+fig.savefig('001_compare_wakes'+adaptative*'_adaptative'+'.png')
 
 # Compare impedances
 
-f_de = np.linspace(0, 5e9, 10000)
+f_de = np.linspace(1, 5e9, 10000)
 Z_de = DE_model.get_impedance(frequency_data=f_de, wakelength=None) 
+time = np.linspace(0, 50e-9, 1000)
 
 f_fft, Z_fft = iddefix.compute_fft(time, W_de/c_light, fmax=5e9) 
-f_nft, Z_nft = iddefix.compute_fft(time, W.imag/c_light/np.pi, fmax=5e9) 
+
+f_inft, Z_inft = iddefix.compute_fft(time, W/c_light, fmax=5e9) 
+
+f_nft, Z_nft = iddefix.compute_neffint(time, DE_model.get_wake(time),
+                                 frequencies=f_de,
+                                 adaptative=adaptative)
 
 Z_fft *= 1j #transverse
+Z_inft *= 1j #transverse
 Z_nft *= 1j #transverse
 
 fig = plt.figure(figsize=(12, 7))
@@ -126,13 +102,17 @@ plt.plot(f_fft, np.real(Z_fft), color='tab:red', ls='--', label='numpy FFT real 
 plt.plot(f_fft, np.imag(Z_fft), color='tab:blue', ls='--', label='numpy FFT imag. impedance')
 plt.plot(f_fft, np.abs(Z_fft), color='tab:green', ls='--', label='numpy FFT Abs. impedance')
 
+#plt.plot(f_inft, np.real(Z_inft), color='tab:red', ls=':', label='iNeffint real impedance')
+#plt.plot(f_inft, np.imag(Z_inft), color='tab:blue', ls=':', label='iNeffint imag. impedance')
+#plt.plot(f_inft, np.abs(Z_inft), color='tab:green', ls=':', label='iNeffint Abs. impedance')
+
 plt.plot(f_nft, np.real(Z_nft), color='tab:red', ls=':', label='Neffint real impedance')
-plt.plot(f_nft, np.imag(Z_nft), color='tab:blue', ls=':', label='Neffint imag. impedance')
-plt.plot(f_nft, np.abs(Z_nft), color='tab:green', ls=':', label='Neffint Abs. impedance')
+plt.plot(f_nft, np.imag(Z_nft), color='tab:blue',  ls=':', label='Neffint imag. impedance')
+plt.plot(f_nft, np.abs(Z_nft), color='tab:green',  ls=':', label='Neffint Abs. impedance')
 
 plt.legend()
 plt.xlabel('f [Hz]')
 plt.ylabel('$Z_{Transverse}$ [$\Omega$]')
 plt.show()
-fig.savefig('001_compare_imp.png')
+fig.savefig('001_compare_imp'+adaptative*'_adaptative'+'.png')
 
