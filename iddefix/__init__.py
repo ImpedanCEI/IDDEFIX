aa0322896248bf2c30be982240d9fef48e31c37a
elenafuengar <elena.delafuenteg@gmail.com>
2025-07-22 12:19:27 +0200

    build: expose newly added functions

--- iddefix/__init__.py
+++ iddefix/__init__.py
@@ -10,6 +10,6 @@ from .objectiveFunctions import ObjectiveFunctions
 from .resonatorFormulas import Wakes, Impedances
 from .smartBoundDetermination import SmartBoundDetermination
 
-from .utils import compute_fft, compute_deconvolution
+from .utils import compute_fft, compute_deconvolution, compute_convolution, gaussian_bunch
 from .utils import compute_ineffint, compute_neffint
 from ._version import __version__
